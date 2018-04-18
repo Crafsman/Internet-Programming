6ef47eca9d0154f247f0c9752d51424a2e8f5ce8
Crafsman <ymq655@gmail.com>
2018-04-18 14:15:00 +1000

    query product ID and display on right-top <iframe>

--- ass1/top-right.php
+++ ass1/top-right.php
@@ -1,9 +1,34 @@
 <?php
 
 if (isset($_REQUEST['data'])) {
-	echo $_REQUEST['data'];
+	// If receive products ID then, retrive this ID in database
+	$ID = $_REQUEST['data'];
+	
+	//Procedural style
+	$connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
+
+	$query_string = "select * from films where (product_id = $ID)";
+	 
+	$result=mysqli_query($connection,$query_string);
+	 
+	$num_rows=mysqli_num_rows($result);
+	if ($num_rows > 0 ) {
+		print "<table border='0'>";
+		while ( $a_row = mysqli_fetch_array($result)){
+			 print "<tr>\n";
+			 foreach ($a_row as $field)
+				 print "\t<td>$field</td>\n";
+			 print "</tr>";
+		}
+		print "</table>";
+	}
+	mysqli_close($link);
+
+
+
+	
 } else {
-	echo "This is top-right.";
+	echo "No data sent to this page.";
 }
 
 
