f78593a140cbf29229cf51e648dfc266f71ff350
Crafsman <ymq655@gmail.com>
2018-04-23 23:21:12 +1000

    finish shopping cart add function, clear functoins, and total price funcitons.

--- ass1/bottom-right.php
+++ ass1/bottom-right.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-echo $_SESSION["currentID"];
+
+if ($_REQUEST["clear"] == 1) {
+  unset ($_SESSION['products']);
+  unset($_SESSION["currentProduct"]);
+}
+if(isset($_SESSION["currentProduct"])){
+  $_SESSION["products"][] = $_SESSION["currentProduct"];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,31 +18,47 @@ echo $_SESSION["currentID"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php 
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\" />";
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/bottom-right.css\" />";
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/mystyle.css\" />";
+    ?>
 </head>
 <body>
-<?php
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\" />";
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/bottom-right.css\" />";
 
-?>
-<button>Default Button</button>
-<a href="#" class="button">Link Button</a>
-<button class="button">Button</button>
-<input type="button" class="button" value="Input Button">
+
+<div class="linkbtn">
+	<a href="bottom-right.php?clear=1"   target="bottom-right" type="button" >
+	Clear
+	</a>
+</div>
+
+
 
 <hr>
 
 <div class="row">
 
-<div class="col-25">
+<div class="col-25" >
   <div class="container">
-    <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4..</b></span></h4>
-    <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-    <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-    <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-    <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+    <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i><b><?php echo count($_SESSION["products"]);?></b></span></h4>
+  
+  
+  <?php 
+    foreach($_SESSION["products"] as $product){ ?>
+      <p><a href="#"><?php echo $product["product_name"];?></a><span class="price">$<?php echo $product["unit_price"];?></span></p>
+  <?php } ?>
+
     <hr>
-    <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+
+    <p>Total <span class="price" style="color:black"><b>$
+    <?php
+    $total = 0;
+    foreach($_SESSION["products"] as $product){
+      $total += $product["unit_price"];
+    }
+    echo $total;
+    ?></b></span></p>
   </div>
 </div>
 </div>
--- ass1/top-right.php
+++ ass1/top-right.php
@@ -1,9 +1,7 @@
 <?php
 // Start the session
 session_start();
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,59 +10,61 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-
+	<?php echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/mystyle.css\" />"; ?>
 </head>
 <body>
 
+
+
 <?php
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/mystyle.css\" />";
 
-if (isset($_REQUEST['data'])) {
-	// If receive products ID then, retrive this ID in database
-	$ID = $_REQUEST['data'];
 
-	// Store current product ID in session;
-	$_SESSION["currentID"] = $ID;
+
+	if (isset($_REQUEST['data'])) {
+		// If receive products ID then, retrive this ID in database
+		$ID = $_REQUEST['data'];
 	
-	//Procedural style
-	$connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
-
-	$query_string = "select * from products where (product_id = $ID)";
-	 
-	$result=mysqli_query($connection,$query_string);
-	 
-	$num_rows=mysqli_num_rows($result);
-
-	if ($num_rows > 0 ) {
+		// Store current product ID in session;
+		$_SESSION["currentID"] = $ID;
 		
-		if ( $a_row = mysqli_fetch_array($result)){
+		//Procedural style
+		$connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
+	
+		$query_string = "select * from products where (product_id = $ID)";
+		 
+		$result=mysqli_query($connection,$query_string);
+		 
+		$num_rows=mysqli_num_rows($result);
+	
+		if ($num_rows > 0 ) {
 			
-			print "<br>";		
-			{
+			if ( $a_row = mysqli_fetch_array($result))
+			{	
+				print "<br>";		
+				
 				echo $a_row[product_id]."<br>";
 				echo $a_row[product_name]."<br>";
 				echo $a_row[unit_price]."<br>";
 				echo $a_row[unit_quantity]."<br>";
 				echo $a_row[in_stock]."<br>";				
+				
+				
+				$_SESSION["currentProduct"] = $a_row;
+		
+				
 			}
 		}
-
-		// display a button here ?
-		// print "<form id=\"myForm\" method=\"post\" action=\"bottom-right.php\">";
-		// print "<input type=\"submit\" id=\"add-button\" value=\"ADD\" class=\"add-button\" >";
+		mysqli_close($link);
 	
-		// print "</form>";
+	
+	
+	} else {
+		echo "No data sent to this page.";
 	}
-	mysqli_close($link);
-	
-} else {
-	echo "No data sent to this page.";
-}
+
+
 ?>
-<!-- <form action="bottom-right.php" method="post" onsubmit="return gohome()">
-	<input type="submit" class="add-button" value="ADD" >
-</form> -->
-<!-- <button type="button" class="add-button" href="bottom-right.php"  target="bottom-right">ADD</button> -->
+
 <div class="linkbtn">
 	<a href="bottom-right.php"  id="addbtn" target="bottom-right" type="button" class="add-button">
 	ADD
