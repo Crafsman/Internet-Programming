4bc2acdee03e624901cbe5a4015595e6bde6e185
Crafsman <ymq655@gmail.com>
2018-05-06 15:25:16 +1000

    1. Email validate 2. no frameborder; 3 add red asterisk

--- ass1/checkoutform.php
+++ ass1/checkoutform.php
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <div class="row">
   <div class="col-75">
@@ -6,23 +7,36 @@
       
         <div class="row">
           <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe" required>
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com" required>
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <h3>Contact Details</h3>
+            <label for="fname"><i class="fa fa-user"></i> Full Name <span style="color:red">
+*
+</span></label>
+            <input type="text" id="fname" name="firstname" placeholder="John M. Doe"  required >
+            <label for="email"><i class="fa fa-envelope"></i> Email <span style="color:red">
+*
+</span></label>
+            <label id="validate_email_message" style="color:red"></label>
+            <input type="text" id="email" name="email" placeholder="john@example.com" onblur="validate()" required>
+            <label for="adr"><i class="fa fa-address-card-o"></i> Address <span style="color:red">
+*
+</span></label>
             <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" required>
-            <label for="city"><i class="fa fa-institution"></i> Suburb</label>
+            <label for="city"><i class="fa fa-institution"></i> Suburb <span style="color:red">
+*
+</span></label>
             <input type="text" id="city" name="city" placeholder="Rockdale" required>
 
             <div class="row">
               <div class="col-50">
-                <label for="state">State</label>
+                <label for="state">State <span style="color:red">
+*
+</span></label>
                 <input type="text" id="state" name="state" placeholder="NSW" required>
               </div>
               <div class="col-50">
-                <label for="country">Country</label>
+                <label for="country">Country <span style="color:red">
+*
+</span></label>
                 <input type="text" id="country" name="country" placeholder="Australia" required>
               </div>
             </div>
@@ -34,7 +48,42 @@
       </form>
     </div>
   </div>
-  
 
 </div>
 
+<script type="text/javascript">
+
+
+
+$(document).ready(function(){
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+
+    $("#email").blur(function(){
+      var $result = $("#validate_email_message");
+      var email = $("#email").val();
+      $result.text("");
+      $result.hide();
+
+    if (validateEmail(email)) {
+      // $result.text(email + " is valid :)");
+      // $result.css("color", "green");
+      $result.hide();
+    } else {
+      $result.show();
+      $result.text(email + " is not valid :(");
+      $result.css("color", "red");
+    }
+
+
+        // $(this).css("background-color", "#aaaaaa");
+        // $('#validate_email_message').text('invalid email');
+    });
+});
+
+</script>
+</script>
