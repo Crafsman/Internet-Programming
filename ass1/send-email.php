58ce35754edfb50b15415324fdab49a6ae5b48c0
Crafsman <ymq655@gmail.com>
2018-05-08 16:34:26 +1000

    finish email

--- ass1/send-email.php
+++ ass1/send-email.php
@@ -1,20 +1,19 @@
 <?php
 
-echo "test";
-
-echo $_REQUEST['firstname'];
-echo $_REQUEST['email'];
-echo $_REQUEST['address'];
-echo $_REQUEST['city'];
-echo $_REQUEST['state'];
-echo $_REQUEST['country'];
 
 
+// echo $_REQUEST['firstname'];
+// echo $_REQUEST['email'];
+// echo $_REQUEST['address'];
+// echo $_REQUEST['city'];
+// echo $_REQUEST['state'];
+// echo $_REQUEST['country'];
 
+echo "Your order is sent to: <b>".$_REQUEST['email']."</b> Please check it";
 
-
+$msg = "Hello ".$_REQUEST['firstname'].","."This is your invoice.";
 //echo $mgs;
 
-mail($_REQUEST['email'],"purchase invoice","test");
+mail($_REQUEST['email'],"purchase invoice",$msg);
 
 ?>
